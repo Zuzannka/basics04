4d27ba92c6c20eabec5ed1310cc84bf4772c6015
Zuzanka <iglewskaz@gmail.com>
2020-05-06 21:49:59 +0200

    lista.py

---
+++ lista.py
@@ -0,0 +1,13 @@
+list_a = [1, 2, 3, 4, 5, 6, 7]
+print(list_a[0:7:2])
+print(list_a[1:7:2])
+list_a.reverse()
+print(list_a)
+print(list_a[1:7:2])
+print(list_a[0:7:6])
+print(list_a[0:7:3])
+list_a.clear()
+print(list_a)
+list_a = [5,6,7]
+print(list_a)
+#jestem prawie pewna że nie o to chodziło w ostatnim ale nie wiem jak to lepiej zrobić
